0e129c86b3517350e7a3a8546f4b171063754531
SANTOSH <santosh.3839@gmail.com>
2026-01-05 12:23:18 +0530

    Updated the Controller class

--- src/main/java/com/javatechie/CourseServiceApplication.java
+++ src/main/java/com/javatechie/CourseServiceApplication.java
@@ -2,7 +2,7 @@ package com.javatechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Swagger link: http://13.203.76.225:8080/swagger-ui/index.html
 @SpringBootApplication
 public class CourseServiceApplication {
 
